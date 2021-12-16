298d4dff9fd3c8ab697afccf04a795cd62c7d9e1
Darius MOREL <D21125495@mytudublin.ie>
2021-12-16 04:46:57 +0100

    integrate product

--- src/function.php
+++ src/function.php
@@ -19,38 +19,51 @@ function getHtml($name)
 function productToHtml()
 {
     $pc = new ProductController();
-
+    $id = 10000;
+    $html = "";
     $obj_list = $pc->getProducts();
-    $html;
-    #echo var_dump($obj_list);
+    for ($i = 0; $i < count($obj_list); $i++) {
+        $obj_list[$i];
+        $name = $obj_list[$i]->getName();
+        $stock = $obj_list[$i]->getStock();
+        $category = $obj_list[$i]->getType();
+        $price = $obj_list[$i]->getPrice();
+        $vat = 0;
+        if ($category == "Luxury") {
+            $vat = 20;
+        } elseif ($category == "Essential") {
+            $vat = 10;
+        } elseif ($category == "Gift") {
+            $vat = 5;
+        }
 
-    /*
-    $product1 = new Product("Rings", "In stock", "15/12/2021", "50", "Luxury");
-    $product2 = new Product("Jewelry", "Out of stock", "31/06/2022", "50", "Luxury");
-    $product3 = new Product("Necklaces", "In stock", "21/08/2025", "50", "Luxury");
-    $product4 = new Product("Apple", "In stock", "24/02/2035", "30", "Essential");
-    $product5 = new Product("Pear", "In stock", "24/03/2035", "30", "Essential");
-    $product6 = new Product("Spoon", "In stock", "24/05/2035", "30", "Essential");
-    $product7 = new Product("Candle", "Out of stock", "03/11/2022", "20", "Gift");
-    $product8 = new Product("Perfume", "Out of stock", "23/11/2022", "20", "Gift");
-    $pc = new ProductController();
-    $pc->addProduct($product1);
-    $pc->addProduct($product2);
-    $pc->addProduct($product3);
-    $pc->addProduct($product4);
-    $pc->addProduct($product5);
-    $pc->addProduct($product6);
-    $pc->addProduct($product7);
-    $pc->addProduct($product8);
-    */
-    // $stor = new Storage("products");
-    // $stor->setContent($product->getProduct());
-    // $tmp = array($stor->getContent());
-    // array_push($tmp, $product->getProduct());
-    // $stor->setContent($tmp);
-    // $stor->writeStorage();
-    
-    return ("<div>BONJOUR</div> ");
+        $item = <<<EOT
+        <tr id="$id" style="display:none">
+            <td class="col-sm-8 col-md-6">
+                <div class="media">
+                    <a class="thumbnail pull-left" href="#"> <img class="media-object" src="http://icons.iconarchive.com/icons/custom-icon-design/flatastic-2/72/product-icon.png" style="width: 72px; height: 72px;"> </a>
+                    <div class="media-body">
+                        <h4 class="media-heading"><a href="#">$name</a></h4>
+                        <h5 class="media-heading"><strong  id="{$id}_table_category">$category</strong></h5>
+                        <span>Status: </span><span class="text-success"><strong>$stock</strong></span>
+                    </div>
+                </div>
+            </td>
+        
+            <td class="col-sm-1 col-md-1 text-center"><input type='number' id="{$id}_table_quantity" value=1></span></td>
+            <td class="col-sm-1 col-md-1 text-center"><span id="${id}_table_price">$price</span>€</td>
+            <td class="col-sm-1 col-md-1 text-center"><span id="${id}_table_vat">$vat</span>%</td>
+            <td class="col-sm-1 col-md-1">
+                <button type="button" class="btn btn-success" onclick="add_item('$name', '$category', '$stock', '${id}_table_quantity', '$price', '$vat')">
+                    <span class="glyphicon glyphicon-plus"></span> Add to basket
+                </button>
+            </td>
+        </tr>
+        EOT;
+        $html .= $item;
+        $id += 1;
+    }
+    return ($html);
 }
 function fillPage($body)
 {
